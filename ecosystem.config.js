efb4c7287ee105350b4f953a84b526ec2ff364a0
王婷婷 <2372454518@qq.com>
2019-02-18 17:59:09 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,8 @@ module.exports = {
     script: 'orderWebServer.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
     watch: true,
     env: {
       NODE_ENV: 'development'
