0aec864e48a3eedffaedc06e9b5b88617dcde915
王婷婷 <2372454518@qq.com>
2019-02-18 18:26:36 +0800

    update

--- orderWebServer.js
+++ orderWebServer.js
@@ -16,16 +16,16 @@ const convert = require('koa-convert');
 const app = new Koa();
 app.keys = ['this is my secret and fuck you all'];
 
-app.all('*', async (ctx, next) => {
+// app.all('*', async (ctx, next) => {
 
-  ctx.set("Access-Control-Allow-Origin", "*")
-  ctx.set("Access-Control-Allow-Headers", "X-Requested-With")
-  ctx.set("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS")
-  ctx.set("X-Powered-By",' 3.2.1')
-  ctx.set("Content-Type", "application/json;charset=utf-8")
-  next()
+//   ctx.set("Access-Control-Allow-Origin", "*")
+//   ctx.set("Access-Control-Allow-Headers", "X-Requested-With")
+//   ctx.set("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS")
+//   ctx.set("X-Powered-By",' 3.2.1')
+//   ctx.set("Content-Type", "application/json;charset=utf-8")
+//   next()
 
-})
+// })
 
 
 app.use(cors({
