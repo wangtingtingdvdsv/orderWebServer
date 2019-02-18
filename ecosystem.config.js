9287535f870218bc5fb54e90cd369f928e2185fe
王婷婷 <2372454518@qq.com>
2019-02-18 18:04:02 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,8 @@ module.exports = {
     args: 'one two',
     instances: 1,
     autorestart: true,
-    watch: true,
+    watch: false,
+    max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development'
     },
